7f359b3190b8a9e45f9f42d540a6634e6d48583f
Heverson Silva Vasconcelos <heverson.vasconcelos@gmail.com>
2022-08-30 10:51:25 -0400

    first commit

---
+++ src/main/java/br/gov/sp/prodesp/pubnet/PubnetApplication.java
@@ -0,0 +1,13 @@
+package br.gov.sp.prodesp.pubnet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PubnetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PubnetApplication.class, args);
+	}
+
+}
